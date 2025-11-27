03f12526a78dc927234957e2ccf77413033d7eba
Pakawun <pakawun.jid@student.mahidol.edu>
2025-11-28 03:00:43 +0700

    fix add machine function

--- Sales_system/get_all_products.sql
+++ Sales_system/get_all_products.sql
@@ -1,17 +1,22 @@
 create function get_all_products()
-    returns TABLE(product_id integer, product_name character varying, price numeric, inventory_amount integer)
+    returns TABLE
+            (
+                product_id       integer,
+                product_name     character varying,
+                price            numeric,
+                inventory_amount integer
+            )
     language plpgsql
 as
 $$
 BEGIN
-    RETURN QUERY
-        SELECT
-            p.product_id,
-            p.product_name,
-            p.price,
-            i.quantity AS inventory_amount
-        FROM product p
-                 LEFT JOIN inventory i ON p.inventory_id = i.inventory_id;
+RETURN QUERY
+SELECT p.product_id,
+       p.product_name,
+       p.price,
+       i.quantity AS inventory_amount
+FROM product p
+         LEFT JOIN inventory i ON p.inventory_id = i.inventory_id;
 END;
 $$;
 
