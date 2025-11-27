0e28abda257428df6a7f8166163f621065103ac9
Pakawun <pakawun.jid@student.mahidol.edu>
2025-11-28 00:32:19 +0700

    list all product function

---
+++ Sales_system/get_all_products.sql
@@ -0,0 +1,19 @@
+create function get_all_products()
+    returns TABLE(product_id integer, product_name character varying, price numeric, inventory_amount integer)
+    language plpgsql
+as
+$$
+BEGIN
+    RETURN QUERY
+        SELECT
+            p.product_id,
+            p.product_name,
+            p.price,
+            i.quantity AS inventory_amount
+        FROM product p
+                 LEFT JOIN inventory i ON p.inventory_id = i.inventory_id;
+END;
+$$;
+
+alter function get_all_products() owner to coffeedb_zgpm_user;
+
