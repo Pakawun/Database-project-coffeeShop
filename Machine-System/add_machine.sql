90592e8f3d9bd5866041c34d8eb975cdce61626f
Pakawun <pakawun.jid@student.mahidol.edu>
2025-11-30 01:25:36 +0700

    update add machine function

--- Machine-System/add_machine.sql
+++ Machine-System/add_machine.sql
@@ -1,25 +1,15 @@
-create function add_machine(p_machine_name character varying, p_machine_type character varying, p_purchase_date date, p_status character varying, p_serial_number character varying, p_warranty_expired_date date) returns integer
+create function add_machine(p_owner_id integer, p_machine_name character varying, p_machine_type character varying, p_purchase_date date, p_status character varying, p_serial_number character varying, p_warranty_expired_date date) returns integer
     language plpgsql
 as
 $$
 DECLARE
     new_id INT;
 BEGIN
-    INSERT INTO machine (
-        machine_type,
-        purchase_date,
-        status,
-        warranty_expired_date,
-        serial_number,
-        machine_name
+    INSERT INTO public.machine (
+        owner_id, machine_name, machine_type, purchase_date, status, serial_number, warranty_expired_date
     )
     VALUES (
-               p_machine_type,
-               p_purchase_date,
-               p_status,
-               p_warranty_expired_date,
-               p_serial_number,
-               p_machine_name
+               p_owner_id, p_machine_name, p_machine_type, p_purchase_date, p_status, p_serial_number, p_warranty_expired_date
            )
     RETURNING machine_id INTO new_id;
 
@@ -27,5 +17,5 @@ BEGIN
 END;
 $$;
 
-alter function add_machine(varchar, varchar, date, varchar, varchar, date) owner to coffeedb_zgpm_user;
+alter function add_machine(integer, varchar, varchar, date, varchar, varchar, date) owner to coffeedb_zgpm_user;
 
