a187d9573b3edf37f43bba0691c387623a302473
Pakawun <pakawun.jid@student.mahidol.edu>
2025-11-30 01:21:27 +0700

    update the add coupon function

--- Sales_system/add_coupon.sql
+++ Sales_system/add_coupon.sql
@@ -1,15 +1,15 @@
-create function add_coupon(p_coupon_code character varying, p_discount_value integer, p_valid_from date, p_valid_to date) returns integer
+create function add_coupon(p_owner_id integer, p_coupon_code character varying, p_discount_value integer, p_valid_from date, p_valid_to date) returns integer
     language plpgsql
 as
 $$
 DECLARE
     new_id INT;
 BEGIN
-    INSERT INTO coupon(
-        coupon_code, discount_value, valid_from, valid_to
+    INSERT INTO public.coupon(
+        owner_id, coupon_code, discount_value, valid_from, valid_to
     )
     VALUES (
-               p_coupon_code, p_discount_value, p_valid_from, p_valid_to
+               p_owner_id, p_coupon_code, p_discount_value, p_valid_from, p_valid_to
            )
     RETURNING coupon_id INTO new_id;
 
@@ -17,5 +17,5 @@ BEGIN
 END;
 $$;
 
-alter function add_coupon(varchar, integer, date, date) owner to coffeedb_zgpm_user;
+alter function add_coupon(integer, varchar, integer, date, date) owner to coffeedb_zgpm_user;
 
